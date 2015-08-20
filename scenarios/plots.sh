3e48ba581e01817e1b22a7031e60326430304854
Matthew Gidden <matthew.gidden@gmail.com>
2015-08-20 14:54:54 -0500

    group by prototype

--- scenarios/plots.sh
+++ scenarios/plots.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-db=dre.sqlite
+db=cyclus.sqlite
+simid= # defaults to first in db
 
-# simid= # defaults to first in db
+#db=dre.sqlite
 # simid=c2560eaf-6637-42ad-817f-8961623dbb36 # recycle
-simid=ab7d15ed-306f-48bb-b2c1-39dd474e4fae # base_case
+# simid=ab7d15ed-306f-48bb-b2c1-39dd474e4fae # base_case
 
 # split db in two, take first item as name for other files
 IFS='.' read -ra FOO <<< $db
@@ -14,7 +15,7 @@ echo working on $db
 
 cyan -db $db post
 
-cyan -db=$db -simid=$simid -simid=$simid flowgraph > flow.dot
+cyan -db=$db -simid=$simid -simid=$simid flowgraph -proto > flow.dot
 dot -Tpng -o "$name"_flow.png flow.dot
 
 cyan -db=$db -simid=$simid built -p reactor
