d13ed577defaa3004a48ef77c3b0e3631d055713
Matthew Gidden <matthew.gidden@gmail.com>
2015-09-22 17:30:59 -0500

    updated run.sh to just run non-cbcs + outage

--- scenarios/run.sh
+++ scenarios/run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-dbs="base_case base_case_cbc military military_cbc tariff tariff_cbc"
 
-for db in $dbs; do
-    echo running $db
-    rm $db.sqlite
-    cyclus $db.xml -o $db.sqlite
-done
+run() {
+    for db in $1; do
+	echo running $db
+	rm $db.sqlite
+	cyclus $db.xml -o $db.sqlite
+    done
+}
+
+dbs="base_case military tariff outage "
+run dbs
+
+# dbs="base_case_cbc military_cbc tariff_cbc"
+# run dbs
+
