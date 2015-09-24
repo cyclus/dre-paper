0ca39f0c744e1506acafeb92bd4c928a683e79cc
Matthew Gidden <matthew.gidden@gmail.com>
2015-09-24 16:27:26 -0500

    add cbc dbs to plot.sh

--- scenarios/plots.sh
+++ scenarios/plots.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-dbs="base_case once_through military tariff outage"
+plot() {
+    echo $1
+    for db in $1; do
+	echo $db.sqlite "-> $db"_flow.png
+	cyan -db "$db".sqlite flowgraph -proto > flow.dot
+	dot -Tpng -o figs/"$db"_flow.png flow.dot
+    done
+}
 
-for db in $dbs; do
-    echo $db.sqlite "-> $db"_flow.png
-    cyan -db "$db".sqlite flowgraph -proto > flow.dot
-    dot -Tpng -o figs/"$db"_flow.png flow.dot
-done
+plot "base_case once_through military tariff outage"
+plot "base_case_cbc once_through_cbc military_cbc tariff_cbc outage_cbc"
 
 # cyan -db=$db -simid=$simid built -p reactor
 # cyan -db=$db -simid=$simid inv -p -nucs=Pu239 reactor
