cc4ab53c6fa9266ab79fa4edb918233b24cfa293
Matthew Gidden <matthew.gidden@gmail.com>
2015-09-24 14:00:28 -0500

    adding cbc cases for all and updating old cbc cases with new deployment scheds

--- scenarios/run.sh
+++ scenarios/run.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+set -e
+
 run() {
     echo $1
     for db in $1; do
 	echo running $db
 	rm $db.sqlite
-	cyclus $db.xml -o $db.sqlite
+	cyclus $db.xml -o $db.sqlite &> out
+	rm out
     done
 }
 
 run "base_case once_through military tariff outage"
 
-# run "base_case_cbc military_cbc tariff_cbc"
+run "base_case_cbc once_through_cbc military_cbc tariff_cbc outage_cbc"
 
